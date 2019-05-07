aa55fa53791d255e3cbe22a9e6a1498df0f239a8
HullarJozsef <35250111+HullarJozsef@users.noreply.github.com>
2019-05-07 06:27:52 +0200

    Javascript alapok és működés

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Módosítjuk a h1 elem taratlmát.
+document.querySelector("h1").innerHTML = "Hello Belló";
+
+//Kép forrásának módosítása
+function changeUrl() {
+    document.querySelector("img").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgi85f0UjO9MCwOmRakCQWxCnMJ-agvkGuOfao7iA16DnJudGb";    
+}
+
+//Stílus módosítása
+function changeStyle() {
+    document.querySelector("img").style.width = "40px";    
+}
